8f77c824c1a44fab16ddb412e6b9bf12d4852e63
Weihao Wu <weihaowu738@gmail.com>
2026-09-25 19:48:26 -0400

    refactored backend from django to spring boot

---
+++ backend/src/test/java/edu/rpi/backtests/BacktestsApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.rpi.backtests;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BacktestsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
