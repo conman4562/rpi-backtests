966889a1c3402103a6825dd9d00c4ef7ccc6877b
smallgiant1010 <smallgiant1010@gmail.com>
2026-09-25 22:45:35 -0400

    switched to spring boot, and edited config files including setting up dockerfiles

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,4 +4,14 @@ import { defineConfig } from 'vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    host: "0.0.0.0",
+    port: parseInt(process.env.VITE_APP_PORT || '5173'),
+    proxy: {
+      '/api': {
+        target: process.env.API_PROXY_TARGET || 'http://backend:8080',
+        changeOrigin: true,
+      },
+    },
+  },
+});
